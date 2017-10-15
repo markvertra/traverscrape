042cfe81f31b0d96fb892231917ba7a1d843855c
Mark Allen <mark@traversify.com>
2017-10-15 14:54:24 +0200

    can load basic GYG and Viator pages

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const cheerio = require('cheerio');
+const jsonframe = require('jsonframe-cheerio');
+const request = require('request');
+
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index');
+router.get('/viator/:city', (req, res, next) => {
+  request("http://www.viator.com/" + req.params.city, (request, response, body)=> {
+    const $ = cheerio.load(body);
+    item = $("body").html();
+    res.send(body);
+  });
+});
+
+router.get('/getyourguide/:city', (req, res, next) => {
+  request("http://www.getyourguide.com/" + req.params.city, (request, response, body)=> {
+    const $ = cheerio.load(body);
+    item = $("body").html();
+    res.send(body);
+  });
 });
 
 module.exports = router;
