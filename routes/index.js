7a7bb77d9200708a2009c77b2eb2ee247009c1cc
Mark Allen <mark@traversify.com>
2017-10-15 16:38:52 +0200

    get your guide page complete

--- routes/index.js
+++ routes/index.js
@@ -14,32 +14,31 @@ router.get('/viator/:city', (req, res, next) => {
   //   item = $("body").html();
   //   res.json(body);
   // });
-  const $ = cheerio.load(fs.readFileSync('/Users/markallen/Documents/Projects/Travescrape/examples/viator-example.htm'));
+  let $ = cheerio.load(fs.readFileSync('/Users/markallen/Documents/Projects/Travescrape/examples/viator-example.htm'));
   let units = []
   $('.unit').each((i, elem) => {
-    units[i] = $(this).html();
+    units[i] = elem
   })
   res.json(units);
 });
 
 router.get('/gyg/:city', (req, res, next) => {
-  // request("http://www.getyourguide.com/" + req.params.city, (request, response, body)=> {
-  //   const $ = cheerio.load(body);
-  //   item = $(".body").html();
-  //   res.json(body);
-  // });
-  const $ = cheerio.load(fs.readFileSync('/Users/markallen/Documents/Projects/Travescrape/examples/gyg-example.htm'));
-  let cards = [];
-  $(".activity-card").each((i, elem) => {
-    card = {}
-    card.number = i + 1;
-    card.title = $(elem).find('.activity-card-title').text();
-    card.price = parseFloat($(elem).find('.price').text().slice(1));
-    card.reviews = parseInt($(elem).find('.rating-total').text().split(' ')[0]);
-    card.duration = $(elem).find('.activity-duration').text().split(' ')[0];
-    cards[i] = card;
-  });
+  // request("http://www.getyourguide.com/s/?q=" + req.params.city, (request, response, body)=> {
+  //   const $ = cheerio.load(body);  
+    let $ = cheerio.load(fs.readFileSync('/Users/markallen/Documents/Projects/Travescrape/examples/gyg-example2.htm'));
+    let cards = [];
+    $(".activity-card").each((i, elem) => {
+      card = {}
+      card.number = i + 1;
+      card.title = $(elem).find('.activity-card-title').text();
+      card.link = $(elem).find('.activity-card-link').attr('href');
+      card.price = parseFloat($(elem).find('.price').text().slice(1));
+      card.reviews = parseInt($(elem).find('.rating-total').text().split(' ')[0]);
+      card.duration = $(elem).find('.activity-duration').text().split(' ')[0];
+      cards[i] = card;
+    });
   res.json(cards);
+  // });
 });
 
 module.exports = router;
