222d042be483c985935d827698a14ac2aa80f2bf
Mark Allen <mark@traversify.com>
2017-10-15 16:14:28 +0200

    getting the top results from GYG

--- routes/index.js
+++ routes/index.js
@@ -3,24 +3,43 @@ const router = express.Router();
 const cheerio = require('cheerio');
 const jsonframe = require('jsonframe-cheerio');
 const request = require('request');
+const fs = require('fs');
 
 
 
 /* GET home page. */
 router.get('/viator/:city', (req, res, next) => {
-  request("http://www.viator.com/" + req.params.city, (request, response, body)=> {
-    const $ = cheerio.load(body);
-    item = $("body").html();
-    res.send(body);
-  });
+  // request("http://www.viator.com/" + req.params.city, (request, response, body)=> {
+  //   const $ = cheerio.load(body);
+  //   item = $("body").html();
+  //   res.json(body);
+  // });
+  const $ = cheerio.load(fs.readFileSync('/Users/markallen/Documents/Projects/Travescrape/examples/viator-example.htm'));
+  let units = []
+  $('.unit').each((i, elem) => {
+    units[i] = $(this).html();
+  })
+  res.json(units);
 });
 
-router.get('/getyourguide/:city', (req, res, next) => {
-  request("http://www.getyourguide.com/" + req.params.city, (request, response, body)=> {
-    const $ = cheerio.load(body);
-    item = $("body").html();
-    res.send(body);
+router.get('/gyg/:city', (req, res, next) => {
+  // request("http://www.getyourguide.com/" + req.params.city, (request, response, body)=> {
+  //   const $ = cheerio.load(body);
+  //   item = $(".body").html();
+  //   res.json(body);
+  // });
+  const $ = cheerio.load(fs.readFileSync('/Users/markallen/Documents/Projects/Travescrape/examples/gyg-example.htm'));
+  let cards = [];
+  $(".activity-card").each((i, elem) => {
+    card = {}
+    card.number = i + 1;
+    card.title = $(elem).find('.activity-card-title').text();
+    card.price = parseFloat($(elem).find('.price').text().slice(1));
+    card.reviews = parseInt($(elem).find('.rating-total').text().split(' ')[0]);
+    card.duration = $(elem).find('.activity-duration').text().split(' ')[0];
+    cards[i] = card;
   });
+  res.json(cards);
 });
 
 module.exports = router;
